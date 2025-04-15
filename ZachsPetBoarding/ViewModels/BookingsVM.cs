87720350d5c08d03f4fc66db87e2958c70e3ddbb
Zach Smith <zachsmith852@gmail.com>
2025-04-14 20:32:09 -0700

    setup bookings form and VM

---
+++ ZachsPetBoarding/ViewModels/BookingsVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ZachsPetBoarding.Models;
+
+namespace ZachsPetBoarding.ViewModels
+{
+    public class BookingsVM
+    {
+        [Required(ErrorMessage = "Please enter a first name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a last name")]
+        public string LastName { get; set; }
+        public string PetName { get; set; }
+
+        [Required(ErrorMessage = "Please choose a check-in date")]
+        public DateTime CheckInDate { get; set; }
+        [Required(ErrorMessage = "Please choose a check-out date")]
+        public DateTime CheckOutDate { get; set; }
+        public decimal TotalCost { get; set; }
+        public DateTime BookingDateTime { get; set; }
+
+    }
+}
